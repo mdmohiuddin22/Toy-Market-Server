326a11654f17c802add22fd15f604881ecab50a0
Md.Mohiuddin <saddammd203@gmail.com>
2023-05-19 19:50:29 -0700

    added cors,express

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const express=require('express');
+const cors =require('cors');
+const app=express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const port =process.env.PORT || 5000;
+app.get('/', (req,res)=>{
+    res.send ("server is running")
+})
+
+
+app.listen(port,()=>{
+    console.log(`ser is running on port:${port}`)
+})
